03070a069d4e129b3db1e3659e6cd97be9e6278d
Lebogang <lbtsatsi18@gmail.com>
2025-11-18 20:48:15 +0200

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FitnessWG.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
